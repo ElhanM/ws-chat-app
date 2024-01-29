c6fac2281786f6c532f225cb9eb88596b254401a
ElhanM <elhan.mahmutovic03@gmail.com>
2024-01-29 11:55:32 +0100

    Add: cors options to backend for both http and ws

--- packages/api/src/main.ts
+++ packages/api/src/main.ts
@@ -1,10 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { log } from '@ws-chat-app/src';
+import { ConfigService } from '@nestjs/config';
+import { Logger } from '@nestjs/common';
+import { SocketIOAdapter } from './socket-io-adapter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+  const logger = new Logger('Main (main.ts)');
+
+  const clientPort = parseInt(configService.get('CLIENT_PORT')) || 3000;
+  const port = parseInt(configService.get('PORT')) || 5000;
+
+  app.enableCors({
+    origin: [
+      `http://localhost:${clientPort}`,
+      new RegExp(`/^http:\/\/192\.168\.1\.([1-9]|[1-9]\d):${clientPort}$/`),
+    ],
+  });
+
+  app.useWebSocketAdapter(new SocketIOAdapter(app, configService));
+
   log('HELLO WORLD');
-  await app.listen(5000);
+
+  await app.listen(port);
+  logger.log(`Server running on port ${port}`);
 }
 bootstrap();
