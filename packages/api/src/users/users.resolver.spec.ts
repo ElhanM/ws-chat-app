669b13e5f381b7046e817a8960d72e2b7d87586f
ElhanM <elhan.mahmutovic03@gmail.com>
2024-02-04 21:58:34 +0100

    Add: basic unit tests for users module; got jest to work

--- packages/api/src/users/users.resolver.spec.ts
+++ packages/api/src/users/users.resolver.spec.ts
@@ -4,16 +4,37 @@ import { UsersService } from './users.service';
 
 describe('UsersResolver', () => {
   let resolver: UsersResolver;
+  let usersService: UsersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersResolver, UsersService],
+      providers: [
+        UsersResolver,
+        {
+          provide: UsersService,
+          useValue: {
+            findAll: jest.fn(),
+            findOne: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     resolver = module.get<UsersResolver>(UsersResolver);
+    usersService = module.get<UsersService>(UsersService);
   });
 
-  it('should be defined', () => {
-    expect(resolver).toBeDefined();
+  it('should find all users', async () => {
+    await resolver.findAll();
+
+    expect(usersService.findAll).toHaveBeenCalled();
+  });
+
+  it('should find one user by username', async () => {
+    const username = 'elhan';
+
+    await resolver.findOne(username);
+
+    expect(usersService.findOne).toHaveBeenCalledWith(username);
   });
 });
--- packages/api/src/users/users.service.spec.ts
+++ packages/api/src/users/users.service.spec.ts
@@ -1,18 +1,73 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { PrismaService } from 'src/prisma.service';
+import * as bcrypt from 'bcrypt';
+
+jest.mock('bcrypt', () => ({
+  hash: jest.fn(
+    () => '$2b$10$1txZNBAeHDSk.QKNOjHCauqJN7bFMTAq0da2Q2OyRKYDfo3mMGcHy',
+  ),
+}));
 
 describe('UsersService', () => {
   let service: UsersService;
+  let prismaService: PrismaService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        // we need to mock all of the stuff injected into the service
+        {
+          provide: PrismaService,
+          useValue: {
+            user: {
+              create: jest.fn(),
+              findMany: jest.fn(),
+              findUnique: jest.fn(),
+            },
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
+    prismaService = module.get<PrismaService>(PrismaService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  it('should create a user', async () => {
+    const createUserInput = {
+      username: 'elhan',
+      password: '1234',
+    };
+
+    await service.create(createUserInput);
+
+    expect(bcrypt.hash).toHaveBeenCalledWith(createUserInput.password, 10);
+    expect(prismaService.user.create).toHaveBeenCalledWith({
+      data: {
+        ...createUserInput,
+        password:
+          '$2b$10$1txZNBAeHDSk.QKNOjHCauqJN7bFMTAq0da2Q2OyRKYDfo3mMGcHy',
+      },
+    });
+  });
+
+  it('should find all users', async () => {
+    await service.findAll();
+
+    expect(prismaService.user.findMany).toHaveBeenCalled();
+  });
+
+  it('should find one user by username', async () => {
+    const username = 'elhan';
+
+    await service.findOne(username);
+
+    expect(prismaService.user.findUnique).toHaveBeenCalledWith({
+      where: {
+        username,
+      },
+    });
   });
 });
