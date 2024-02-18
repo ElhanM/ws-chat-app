6a374258033262d7e58a95db9effe4901a3371c6
ElhanM <elhan.mahmutovic03@gmail.com>
2024-02-18 16:51:03 +0100

    Add: tests for getAllOtherUsers

--- packages/api/src/users/users.resolver.spec.ts
+++ packages/api/src/users/users.resolver.spec.ts
@@ -1,6 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersResolver } from './users.resolver';
 import { UsersService } from './users.service';
+import { createMock } from '@golevelup/ts-jest';
+import { ContextWithAuth } from 'src/types';
+import { Request } from 'express';
 
 describe('UsersResolver', () => {
   let resolver: UsersResolver;
@@ -36,5 +39,30 @@ describe('UsersResolver', () => {
     await resolver.findOne(username);
 
     expect(usersService.findOne).toHaveBeenCalledWith(username);
+  });
+
+  it('should get all other users', async () => {
+    const mockUser = {
+      id: '078d79f4-f4d9-4683-a564-f1d520ba37c1',
+      username: 'elhan',
+    };
+    const mockAuthContext = createMock<ContextWithAuth>();
+    const mockRequest = createMock<Request>();
+    const context = {
+      ...mockAuthContext,
+      req: { ...mockRequest, user: mockUser },
+    };
+    const mockSkip = 0;
+    const mockTake = 15;
+
+    // Mock the getAllOtherUsers method in the UsersService
+    usersService.getAllOtherUsers = jest.fn().mockResolvedValue([]);
+
+    await resolver.getAllOtherUsers(context, mockSkip, mockTake);
+
+    expect(usersService.getAllOtherUsers).toHaveBeenCalledWith(mockUser, {
+      skip: mockSkip,
+      take: mockTake,
+    });
   });
 });
