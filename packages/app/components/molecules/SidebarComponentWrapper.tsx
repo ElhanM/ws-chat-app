0c2f92797401d9d7d92da576998f625e39d51b4b
ElhanM <elhan.mahmutovic03@gmail.com>
2024-02-28 22:05:10 +0100

    Add: transitions to sidebar

--- packages/app/components/molecules/SidebarComponentWrapper.tsx
+++ packages/app/components/molecules/SidebarComponentWrapper.tsx
@@ -9,11 +9,12 @@ type Props = {
 
 const SidebarComponentWrapper = ({ children, noFlex = false }: Props) => {
   const showSidebar = useSelector(selectShowSidebar);
-  console.log({ showSidebar });
 
   return (
     <div
-      className={`${showSidebar ? "block" : "hidden"} md:block w-full sm:w-4/5 md:w-1/4 border border-pale ${noFlex || "flex justify-center items-center md:flex md:justify-center md:items-center"}`}
+      className={`fixed z-10 transform transition-transform duration-500 ease-in-out ${
+        showSidebar ? "translate-x-0" : "-translate-x-full"
+      } md:relative md:translate-x-0 w-full sm:w-4/5 md:w-1/4 border border-pale ${noFlex || "flex justify-center items-center md:flex md:justify-center md:items-center"}`}
     >
       {children}
     </div>
