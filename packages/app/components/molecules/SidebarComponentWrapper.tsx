17f429a7e1f1c497dceae9f7325dcd82049b21b6
ElhanM <elhan.mahmutovic03@gmail.com>
2024-02-28 22:07:13 +0100

    Add: dark mask on rest of layout when sidebar is sm:w-4/5

--- packages/app/components/molecules/SidebarComponentWrapper.tsx
+++ packages/app/components/molecules/SidebarComponentWrapper.tsx
@@ -11,13 +11,18 @@ const SidebarComponentWrapper = ({ children, noFlex = false }: Props) => {
   const showSidebar = useSelector(selectShowSidebar);
 
   return (
-    <div
-      className={`fixed z-10 transform transition-transform duration-500 ease-in-out ${
-        showSidebar ? "translate-x-0" : "-translate-x-full"
-      } md:relative md:translate-x-0 w-full sm:w-4/5 md:w-1/4 border border-pale ${noFlex || "flex justify-center items-center md:flex md:justify-center md:items-center"}`}
-    >
-      {children}
-    </div>
+    <>
+      {showSidebar && (
+        <div className="fixed inset-0 bg-black opacity-50 z-10 md:hidden" />
+      )}
+      <div
+        className={`fixed z-20 transform transition-transform duration-500 ease-in-out ${
+          showSidebar ? "translate-x-0" : "-translate-x-full"
+        } md:relative md:translate-x-0 w-full sm:w-4/5 md:w-1/4 border border-pale ${noFlex || "flex justify-center items-center md:flex md:justify-center md:items-center"}`}
+      >
+        {children}
+      </div>
+    </>
   );
 };
 
